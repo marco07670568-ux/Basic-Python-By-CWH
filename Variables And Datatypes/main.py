64f7205f89ea4a238f362521029c1e54480a10b9
marco07670568-ux <marco07670568@gmail.com>
2026-08-30 13:26:59 +0530

    changes

--- Variables And Datatypes/main.py
+++ Variables And Datatypes/main.py
@@ -1,21 +1,7 @@
-a = 23   
+# This will print hello world
+print("Hello World!") # Something
 
-price = 5.99
-
-name = "Harry"
-city = "Delhi"
-
-is_admin = True 
-is_banned = False
-
-print(type(a))
-
-print(type(price))
-
-print(type(city))
-
-print(type(name))
-
-print(type(is_banned))
-
-print(type(is_admin))
+'''
+This is a 
+multiline comment
+'''
---
+++ Variables And Datatypes/typeconversion.py
@@ -0,0 +1,14 @@
+age = "45"
+
+print(int(age))
+print(int(age) + 5)
+
+price = "1999.43"
+print(float(price)  + 5)
+
+age = 56
+
+print(str(age))
+
+num = "567ddfgdfg"
+print(int(num))
