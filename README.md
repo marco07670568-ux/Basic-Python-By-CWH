c6a8b2d1347ff8ddfd830fb7a8632de3396052ac
marco07670568-ux <marco07670568@gmail.com>
2026-08-30 13:04:45 +0530

    variable and datatypes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Basic-Python-By-CWH
-Better understanding
---
+++ Variables And Datatypes/main.py
@@ -0,0 +1,16 @@
+a = 23   
+
+price = 5.99
+
+name = "Harry"
+city = "Delhi"
+
+is_admin = True 
+is_banned = False
+
+print(type(a))
+print(type(price))
+print(type(city))
+print(type(name))
+print(type(is_banned))
+print(type(is_admin))
