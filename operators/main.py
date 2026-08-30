0ecb8ab1af95f8d548a5d171d995740f0d955906
marco07670568-ux <marco07670568@gmail.com>
2026-08-30 20:28:44 +0530

    operators

---
+++ operators/main.py
@@ -0,0 +1,29 @@
+a = 10 # Assignment Operator
+b = 3
+
+# Arithmetic Operators
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+# Comparison Operators
+print(a == b)
+print(a != b)
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b) 
+
+a +=4 
+print(a)
+
+print(False and True)
+print(True and True)
+print(False or True)
+print(True or True)
+print(not(True))
+print(not(False))
