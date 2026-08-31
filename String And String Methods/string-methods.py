58c2c46f682d37669e1483dea481b030e0a56606
marco07670568-ux <marco07670568@gmail.com>
2026-08-31 15:46:24 +0530

    slicing indexing

---
+++ String And String Methods/string-methods.py
@@ -0,0 +1,13 @@
+name = "HarryHar"
+
+# name[0] = "G" # This is not allowed
+print(name)
+print(len(name))
+print(name.lower()) 
+print(name.upper()) 
+print(len(name.strip()))
+
+print(name.replace("Har", "Gur"))
+
+print(name.isalpha())
+print(name.isnumeric())
---
+++ String And String Methods/strings.py
@@ -0,0 +1,16 @@
+name = "Harry"
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5]) # Throws an error
+
+poem = '''Twinkle Twinkle
+little star
+How I wonder
+'''
+
+print(poem)
+print(poem[0])
