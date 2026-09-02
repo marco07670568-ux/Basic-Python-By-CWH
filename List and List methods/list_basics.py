d30daea32f367add9b95a24f51221930e3343f1f
marco07670568-ux <marco07670568@gmail.com>
2026-09-02 23:08:02 +0530

    add initial list examples and methods demonstration

---
+++ List and List methods/list_basics.py
@@ -0,0 +1,14 @@
+names = ["Harry","Larry", "Perry", "Rohan"]
+elements = [1, 34, 67, False, True]
+
+# print(names)
+# print(type(names))
+# print(elements[0])
+# print(elements[1])
+
+print(elements[1:4])
+print(len(elements))
+
+# Lists in Python are mutable
+elements[2] = 69
+print(elements)
---
+++ List and List methods/list_methods.py
@@ -0,0 +1,18 @@
+items = ["apple", "banana", "orange"]
+# print(len(items))
+
+# items.append("strawberries")
+# items.insert(1, "strawberries")
+# items.append(["more bananas", "pineapples"])
+# items.remove("banana")
+# items.pop(1)
+# items.clear()
+# print(items.index("banana"))
+# print(items.count("banana"))
+print(items)
+
+numbers = [11, 1, 4, 5, 55, 78, 2, 9]
+numbers.sort(reverse=True)
+print(numbers)
+
+print(11 in numbers)
